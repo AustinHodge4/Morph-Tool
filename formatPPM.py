862fffdd5c813e7265b3dee55dda72c37b1a85d9
Austin Hodge <hodgeaj4@gmail.com>
2018-08-31 00:08:00 -0400

    start of morphing images

--- formatPPM.py
+++ formatPPM.py
@@ -6,8 +6,10 @@ def read_file(file_path):
     color = ''
     for line in f.readlines():
         if line_count < 3:
+            # First three lines should be the same
             lines.append(line)
         else:
+            # Combine the rgb lines into one line
             if color_count < 3:
                 if color_count == 2:
                     color += line
@@ -29,11 +31,12 @@ def write_file(file_path, lines):
         f.write(line)
     f.close()
 
-def format_images(image_directory, formatted_directory):
+def format_images(image_directory='test-images', formatted_directory='formatted-images'):
     import os
+    # For each file convert to custom ppm style
     for file in os.listdir(image_directory):
         formated_image = read_file(os.path.join(image_directory, file))
         write_file(os.path.join(formatted_directory, file), formated_image)
 
-
+# Convert Gimp styled ppm file to have rgb pairs on same line
 format_images('test-images', 'formatted-images')
--- main.py
+++ main.py
@@ -1,37 +1,84 @@
-def blend_image(left_image_data, right_image_data, steps=1):
-    # Load images
+def blend_image(left_image_data, right_image_data, images, left=0, right=1):
+    # Average images data and create new image
 
-    # Average images
+    image = int((left+right)/2)
 
-    # Create new Image
-    pass
+    # Store image and create more if needed
+    mid = int((left+right)/2)
+    if images[mid]:
+        return images
+
+    images[mid] = image
+    blend_image(images[left], images[mid], images, left, mid)
+    blend_image(images[mid], images[right], images, mid, right)
+
+    return images
 
 def load_images(image_directory):
     def load_image(image_path):
         import re
         f = open(image_path, 'r')
-        line_count = 0
+
         width = None 
         height = None 
         max_brightness = None
+        image_data = None
+
+        width_counter = 0
+        height_counter = 0
+        line_count = 0
+
         for line in f.readlines():
             if line_count == 1:
+                # Width and Height line
+                # Find all groups of numbers
                 m = re.findall(r'\d+', line)
                 width = int(m[0])
                 height = int(m[1])
             if line_count == 2:
+                # Max brightness line
                 max_brightness = int(line)
-            elif line_count > 4:
-                break
+            elif line_count > 2:
+                if image_data == None:
+                    # Create 2D list initialized with 0's
+                    image_data = [[0 for w in range(width)] for h in range(height)]
+
+                # Find all groups of numbers
+                m = re.findall(r'\d+', line)
+                # Set pixel rgb as tuple
+                image_data[height_counter][width_counter] = (int(m[0]), int(m[1]), int(m[2]))
+                width_counter += 1
+                if width_counter == width:
+                    width_counter = 0
+                    height_counter += 1
+
             line_count += 1
-        print("Width: {}\nHeight: {}\nBrightness: {}\n".format(width, height, max_brightness))
+        # print(image_path)
+        # print("Width: {}\nHeight: {}\nBrightness: {}\n".format(width, height, max_brightness))
+        # print("First Pixel: {}".format(image_data[0][0]))
         f.close()
+        return image_data
 
     import os
-    images = []
+    loaded_images = []
+    # Get all files (2) in directory and load them
     for file in os.listdir(image_directory):
-        images.append(load_image(os.path.join(image_directory, file)))
+        loaded_images.append(load_image(os.path.join(image_directory, file)))
 
-    blend_image(images[0], images[1])
+    return load_images
+    
+# Load the two images to morph
+loaded_images = load_images('formatted-images')
 
-load_images('formatted-images')
+# Number of images in between the two provided images
+steps = 2
+images = [loaded_images[0], loaded_images[1]]
+# Create list space to hold the generated in between images
+for x in range(steps):
+    images.insert(x+1, None)
+
+#print(blend_image(images[0], images[steps+1], [10, None, None, 10], right=steps+1))
+# Morph the images
+morphed_images = blend_image(images[0], images[steps+1], images, right=steps+1))
+
+# TODO: Save images as ppm files
